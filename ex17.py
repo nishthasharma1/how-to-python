ab203250803c7d2a14264c40a395533ce1c2c1de
Nishtha Sharma <nishthasharma0516@gmail.com>
2021-05-20 00:05:57 -0600

    i am tired

--- ex17.py
+++ ex17.py
@@ -10,7 +10,7 @@ indata = in_file.read()
 
 print(f"The input file is {len(indata)} bytes long")
 
-print(f"Does the output file exist? {exists(to_file)"})
+print(f"Does the output file exist? {exists(to_file)}")
 print("Ready, hit RETURN to continue, CTRL-C to abort.")
 input()
 
